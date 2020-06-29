1bdd92f5b2a576047fba361c48efe9a5e67fcefb
ivandotv <390700+ivandotv@users.noreply.github.com>
2020-06-29 22:43:00 +0200

    initial commit

---
+++ .babelrc.js
@@ -0,0 +1,60 @@
+const pkg = require('./package.json')
+
+const pkgVersion = process.env.PKG_VERSION || pkg.version
+const ignoreForTests = ['node_modules', 'src/**/*.d.ts']
+const ignoreForProduction = [
+  'src/**/__tests__/**/*',
+  'src/**/*.spec.ts',
+  'src/**/*.test.ts',
+  'src/**/*.d.ts'
+]
+
+module.exports = {
+  presets: ['@babel/typescript', ['@babel/preset-env']],
+  plugins: [
+    ['@babel/plugin-proposal-nullish-coalescing-operator'], //node v10
+    ['@babel/plugin-proposal-optional-chaining'], // node v10
+    ['@babel/proposal-class-properties', { loose: true }], // stage-3 proposal
+    'dev-expression',
+    [
+      'transform-define',
+      {
+        __VERSION__: pkgVersion
+      }
+    ]
+  ],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/env',
+          {
+            targets: {
+              node: 'current'
+            }
+          }
+        ]
+      ],
+      ignore: ignoreForTests,
+      sourceMaps: 'inline'
+    },
+    cjs: {
+      // commonjs for node
+      presets: [
+        [
+          '@babel/env',
+          {
+            // debug: true,
+            modules: 'cjs',
+            targets: {
+              node: 10 // es2018
+            }
+          }
+        ]
+      ],
+      plugins: [['transform-define', browserReplacements]],
+
+      ignore: ignoreForProduction
+    }
+  }
+}
