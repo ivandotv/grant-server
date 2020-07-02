9dcb4e128ef049daa98a8f68d0cb92c5e6ba0151
ivandotv <390700+ivandotv@users.noreply.github.com>
2020-07-02 14:41:55 +0200

    ci(docker): fix docker build and publish workflow

--- docker-image/build.sh
+++ docker-image/build.sh
@@ -6,23 +6,31 @@ username='ivandotv'
 image_name='grant-server'
 build_params="$@"
 full_image_name=${username}/${image_name}
+full_image_name_latest=${full_image_name}:latest
+
+if [[ -n "${GITHUB_WORKFLOW:-}" ]];then
+    # get image tag from ref/tag
+    full_image_name=$full_image_name:${SOURCE_TAG}
+fi
 
 cd docker-image
 
 function clean_up(){
-    rm -rf "$temp_build_dir"
+    rm -rf ${temp_build_dir}
 }
 
 trap clean_up ERR SIGINT
 
-mkdir -p "$temp_build_dir"
+mkdir -p ${temp_build_dir}
 
-cp Dockerfile "$temp_build_dir"
+cp Dockerfile ${temp_build_dir}
 
-cp ../package*.json "$temp_build_dir"
+cp ../package*.json ${temp_build_dir}
 
-cp -R ../dist "temp_build_dir"
+cp -R ../dist ${temp_build_dir}
 
-docker build ${build_params} -t ${full_image_name} ${temp_build_dir}
+docker build ${build_params} -t ${full_image_name} -t ${full_image_name_latest} ${temp_build_dir}
+
+docker push ${username}/${image_name}
 
 clean_up
